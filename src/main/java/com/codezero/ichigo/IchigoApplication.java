5ca962aa47dcbca4101a2ac0df3b330d18b1fb84
code002 <code000two@gmail.com>
2018-09-23 18:28:39 +0800

    Initial commit

---
+++ src/main/java/com/codezero/ichigo/IchigoApplication.java
@@ -0,0 +1,13 @@
+package com.codezero.ichigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class IchigoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IchigoApplication.class, args
+        );
+    }
+}
